89e8c5d86190c49966c58c83870bc89cef3f56d0
hashikawa <hashikawa.yuuichi@gmail.com>
2018-08-05 22:26:33 +0900

    Added colors to each blocks

--- app/src/main/java/com/example/games/TetrisActivity.kt
+++ app/src/main/java/com/example/games/TetrisActivity.kt
@@ -3,10 +3,12 @@ package com.example.games
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TableRow
 import com.example.games.model.Field
+import com.example.games.model.blocks.CompositeBlock
 import com.example.games.model.blocks.IBlock
 import java.util.*
 
@@ -16,14 +18,14 @@ class TetrisActivity : AppCompatActivity() {
     private val _numY = 16
     private val _timeSpan = 1000L
     private val _tetrisField = Field(_numX, _numY, Random(Date().time))
-    private var _currentBlock: IBlock? = null
-    private var _buttons = Array(0) { Array(0) { Button(null) } }
+    private var _currentBlock: CompositeBlock? = null
+    private var _spaces = Array(0) { Array(0) { View(null) } }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tetris)
 
-        _buttons = Array(_numY) { Array(_numX) { Button(this) } }
+        _spaces = Array(_numY) { Array(_numX) { View(this) } }
 
         val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
         val sizeButton = 60
@@ -31,8 +33,8 @@ class TetrisActivity : AppCompatActivity() {
             val tableRow = TableRow(this)
             tableRow.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT)
             for (x in 0 until _numX) {
-                _buttons!![y][x].layoutParams = TableRow.LayoutParams(sizeButton, sizeButton)
-                tableRow.addView(_buttons!![y][x])
+                _spaces!![y][x].layoutParams = TableRow.LayoutParams(sizeButton, sizeButton)
+                tableRow.addView(_spaces!![y][x])
             }
             tableLayout.addView(tableRow, TableLayout.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT))
         }
@@ -77,15 +79,30 @@ class TetrisActivity : AppCompatActivity() {
         runOnUiThread {
             for (y in 0 until _numY) {
                 for (x in 0 until _numX) {
-                    if (_tetrisField.array2d[y][x])
-                        _buttons!![y][x].setBackgroundColor(Color.GRAY)
-                    else
-                        _buttons!![y][x].setBackgroundColor(Color.WHITE)
+                    when (_tetrisField.array2d[y][x]) {
+                        Field.Space.EMPTY -> _spaces!![y][x].setBackgroundColor(Color.WHITE)
+                        Field.Space.RECTANGLE -> _spaces!![y][x].setBackgroundColor(Color.DKGRAY)
+                        Field.Space.STRAIGHT -> _spaces!![y][x].setBackgroundColor(Color.RED)
+                        Field.Space.GAP_LEFT -> _spaces!![y][x].setBackgroundColor(Color.MAGENTA)
+                        Field.Space.GAP_RIGHT -> _spaces!![y][x].setBackgroundColor(Color.YELLOW)
+                        Field.Space.HOOK_LEFT -> _spaces!![y][x].setBackgroundColor(Color.GREEN)
+                        Field.Space.HOOK_RIGHT -> _spaces!![y][x].setBackgroundColor(Color.BLUE)
+                        Field.Space.HOOK_CENTER -> _spaces!![y][x].setBackgroundColor(Color.CYAN)
+                    }
                 }
             }
 
-            for (p in _currentBlock?.positions() ?: arrayOf())
-                _buttons!![p.y][p.x].setBackgroundColor(Color.BLUE)
+            for (p in _currentBlock?.positions() ?: arrayOf()) {
+                when(_currentBlock?.space) {
+                    Field.Space.RECTANGLE -> _spaces!![p.y][p.x].setBackgroundColor(Color.DKGRAY)
+                    Field.Space.STRAIGHT -> _spaces!![p.y][p.x].setBackgroundColor(Color.RED)
+                    Field.Space.GAP_LEFT -> _spaces!![p.y][p.x].setBackgroundColor(Color.MAGENTA)
+                    Field.Space.GAP_RIGHT -> _spaces!![p.y][p.x].setBackgroundColor(Color.YELLOW)
+                    Field.Space.HOOK_LEFT -> _spaces!![p.y][p.x].setBackgroundColor(Color.GREEN)
+                    Field.Space.HOOK_RIGHT -> _spaces!![p.y][p.x].setBackgroundColor(Color.BLUE)
+                    Field.Space.HOOK_CENTER -> _spaces!![p.y][p.x].setBackgroundColor(Color.CYAN)
+                }
+            }
         }
     }
 
