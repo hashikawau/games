0e68b39258b90cddcf4c9913410d918c8b97447a
hashikawa <hashikawa.yuuichi@gmail.com>
2018-08-19 23:56:02 +0900

    Split tetris module

--- app/src/main/java/com/example/games/MainActivity.kt
+++ app/src/main/java/com/example/games/MainActivity.kt
@@ -9,12 +9,16 @@ import com.example.games.tetris.screen.TetrisMenuActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val tetris = "android.intent.action.games.TETRIS"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button_tetris).setOnClickListener { view ->
-            startActivity(Intent(this, TetrisMenuActivity::class.java))
+//            startActivity(Intent(this, TetrisMenuActivity::class.java))
+            val data = Intent(tetris)
+            startActivity(data)
         }
 //        findViewById<Button>(R.id.button_breakblocks).setOnClickListener { view ->
 //            startActivity(Intent(this, BreakBlocksActivity::class.java))
